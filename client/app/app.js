d0b8f44d11a8a30e6f1613e39bcc82df9171ffbe
Cajek <mkucaj86@gmail.com>
2017-04-30 11:52:31 +0200

    add ui bootstrap carousel

--- client/app/app.js
+++ client/app/app.js
@@ -1,18 +1,20 @@
 import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 import angular from 'angular';
+import uiBootstrap from 'angular-ui-bootstrap';
 import uiRouter from 'angular-ui-router';
 import config from './config/app.config';
 import Common from './common/common';
 import ngTranslate from 'angular-translate';
+import angularAnimate from 'angular-animate';
 import 'angular-translate-loader-static-files';
 import AppComponent from './app.component';
 import Components from './components/components';
 import 'bootstrap-sass/assets/javascripts/bootstrap.min.js';
 import 'font-awesome/css/font-awesome.css';
 
-
 angular.module('app', [
   'app.config',
+  uiBootstrap,
   uiRouter,
   Common.name,
   Components.name,
--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,6 +1,26 @@
+import carousel from 'angular-ui-bootstrap/src/carousel';
 class HomeController {
   constructor(User) {
     this.name = 'home';
+    this.carousel = carousel;
+    let currIndex = 0;
+    const tmpSlides = 4;
+    this.myInterval = 2500;
+    this.noWrapSlides = false;
+    this.active = 0;
+    this.slides = [];
+    this.addSlide = function() {
+        const newWidth = 2000 + this.slides.length + 1;
+        this.slides.push({
+            image: '//unsplash.it/' + newWidth + '/500',
+            text: ['Nice image', 'Awesome photograph', 'That is so cool', 'I love that'][this.slides.length % 4],
+            id: currIndex++
+        });
+    };
+    for (let i = 0; i < tmpSlides; i++) {
+        this.addSlide();
+    }
+    console.log(this.slides);
   }
 }
 
